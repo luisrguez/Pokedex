07ab0be3d631c857bd20d8031670f3b7589f63dc
luisrguez <launchx06532@innovaccion.mx>
2022-11-10 11:41:52 -0600

    Accediendo al objeto imagen

--- pokedex.js
+++ pokedex.js
@@ -3,10 +3,12 @@ console.log("Hello Danna");
 const fetchPokemon = () =>{
     const url = `https://pokeapi.co/api/v2/pokemon/ditto`;
     fetch(url).then((res) => {
-        console.log(res);
+        //console.log(res);
         return res.json();
     }).then((data) => {
         console.log(data);
+        let pokeImg = data.sprites.front_default;
+        console.log(pokeImg);
     })
 }
 
